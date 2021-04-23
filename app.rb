b3eb431fd8ee5dbf14f89122c95d313e334b57ac
nova-cairns-nti-johanneberg <60173077+nova-cairns-nti-johanneberg@users.noreply.github.com>
2021-04-23 14:57:19 +0200

    Initial commit

---
+++ app.rb
@@ -0,0 +1,159 @@
+
+require 'sinatra'
+require 'slim'
+require 'sqlite3'
+require 'bcrypt'
+
+get('/') do
+    slim(:index)
+end
+
+get('/admin') do
+  id = session[:id].to_i
+  db = SQLite3::Database.new('databas/databas.db')
+  db.results_as_hash = true 
+  @result = db.execute("SELECT * FROM paket")
+  @result2 = db.execute("SELECT * FROM additions")
+  slim(:admin)
+end
+
+
+get('/login') do
+    slim(:login)
+ end
+
+get('/register') do
+    slim(:register)
+end
+
+get('/konto') do
+  slim(:konto)
+end
+
+get('/kontakt') do
+  slim(:kontakt)
+end
+
+get('/about') do
+  slim(:about)
+end
+
+get('/galleri') do
+  slim(:galleri)
+end
+
+
+
+post('/login') do 
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new('databas/databas.db')
+    db.results_as_hash = true 
+    result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+  
+    if BCrypt::Password.new(pwdigest) == password 
+      if username == "admin" 
+        session[:id] = id
+        redirect('/admin')
+      else 
+      session[:id] = id
+      redirect('/konto')
+      end
+    else 
+      "FEL LÖSEN!"
+    end 
+
+  end 
+  
+  get('/handla') do 
+    id = session[:id].to_i
+    db = SQLite3::Database.new('databas/databas.db')
+    db.results_as_hash = true 
+    result = db.execute("SELECT * FROM paket")
+    slim(:"/handla",locals:{paket:result})
+  end 
+
+  post('/paket') do 
+    paketnamn = params[:paketnamn]
+    id = session[:id].to_i
+    db = SQLite3::Database.new('databas/databas.db')
+    db.execute("INSERT INTO paket (paketnamn) VALUES (?)",paketnamn)
+    redirect('/admin')
+  end 
+
+  post('/additions') do 
+    additionnamn = params[:additionnamn]
+    id = session[:id].to_i
+    db = SQLite3::Database.new('databas/databas.db')
+    db.execute("INSERT INTO additions (additionnamn) VALUES (?)",additionnamn)
+    redirect('/admin')
+  end 
+
+  post('/paket/delete') do 
+    paketnamn = params[:paketnamn]
+    id = session[:id].to_i
+    db = SQLite3::Database.new('databas/databas.db')
+    db.execute("DELETE paket (paketnamn) VALUES (?)",paketnamn)
+    redirect('/admin')
+  end 
+
+  post('/paket/edit') do 
+    paketnamn = params[:paketnamn]
+    id = session[:id].to_i
+    db = SQLite3::Database.new('databas/databas.db')
+    db.execute("UPDATE paket (paketnamn) VALUES (?)",paketnamn)
+    redirect('/admin')
+  end 
+  
+  post('/addition/delete') do 
+    additionnamn = params[:additionnamn]
+    id = session[:id].to_i
+    db = SQLite3::Database.new('databas/databas.db')
+    db.execute("DELETE (additionnamn) VALUES (?)",additionnamn)
+    redirect('/admin')
+  end 
+
+  post('/addition/edit') do 
+    additionnamn = params[:additionnamn]
+    id = session[:id].to_i
+    db = SQLite3::Database.new('databas/databas.db')
+    db.execute("UPDATE  (additionnamn) VALUES (?)",additionnamn)
+    redirect('/admin')
+  end 
+  
+ 
+  
+  
+  post('/users/new') do
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+    if (password == password_confirm)
+      password_digest = BCrypt::Password.create(password)
+      db = SQLite3::Database.new('databas/databas.db')
+      db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
+      redirect('/konto')
+      "Tack för att du registrerade dig"
+    else 
+      "Lösenorden matchade inte"
+    end 
+  end 
+
+  post('/konto') do 
+    username = params[:username]
+    namn = params[:namn]
+    gata = params[:adress]
+    telefon = params[:telefon]
+    postnr = params[:postnr]
+    ort = params[:ort]
+    email = params[:email]
+    db = SQLite3::Database.new('databas/databas.db')
+    db.results_as_hash = true 
+   # result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+    #pwdigest = result["pwdigest"]
+   #id = result["id"]
+   
+  end 
+
---
+++ projektplan.md
@@ -0,0 +1,11 @@
+# Projektplan
+
+## 1. Projektbeskrivning (Beskriv vad sidan ska kunna göra).
+Det är en bokningssida för proffesionella picknicks. Man skapar ett konto för att kunna spara sin påbörjade order. Man väljer ett paket som man vill utgå ifrån. Tillägg finns sedan att lägga till. Man ska även kunna hitta info om företaget osv. 
+## 2. Vyer (visa bildskisser på dina sidor).
+
+## 3. Databas med ER-diagram (Bild på ER-diagram).
+
+## 4. Arkitektur (Beskriv filer och mappar - vad gör/innehåller de?).
+
+
